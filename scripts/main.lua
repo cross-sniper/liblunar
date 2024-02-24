f22b8131975f83cfb2eb7dbe3dc7094c6ef5692f
cross-sniper <vitormirandadossantos3@gmail.com>
2024-02-23 23:32:58 -0300

    deleteded un-needed folder

--- scripts/main.lua
+++
@@ -1,7 +0,0 @@
--- since lua does not have classes, do this
-function init(raylibFunctions)
-	self = {}
-	function self:draw()
-		raylibFunctions.DrawRectangle(self.x, self.y, self.width, self.height, self.color)
-	end
-end
